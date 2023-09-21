66e764c866c67d91f5bd3752a019b698b94b190c
PedroMoraes <pedroomoraes@gmail.com>
2023-09-21 12:18:02 -0300

    Excercício 1002 conforme proposto pelo Beecrowd

--- Beecrowd1002_AreaDoCirculo/Program.cs
+++ Beecrowd1002_AreaDoCirculo/Program.cs
@@ -1,10 +1,19 @@
 ﻿using System;
+using System.Globalization;
 
 namespace beecrowd {
     class Program1002 {
 
         static void Main(string[] args) {
 
+            double raio, n, area;
+
+            n = 3.14159;
+            raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            area = n * (raio * raio);
+
+            Console.WriteLine("A=" + area.ToString("F4", CultureInfo.InvariantCulture));
         }
     }
 }
